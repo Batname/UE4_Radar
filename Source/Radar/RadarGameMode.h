8df84008037305c0628c351a721a7dd9b69363b5
Batname <dadubinin@gmail.com>
2017-06-26 09:42:40 +0200

    init

---
+++ Source/Radar/RadarGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RadarGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARadarGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARadarGameMode();
+};
+
+
+
